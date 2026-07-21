405ded0c34b5dbd640bb59d4988d530f6e66c601
alexrhett2010@gmail.com <alexrhett2010@gmail.com>
2026-07-21 18:52:17 -0400

    Fix TiDB SSL database connection

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-  host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
-  port: 4000,
-  user: '2JryJAvVwJ6Leno.root',
-  password: 'kaRfKal7xZLxcRzJ',
-  database: 'test',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 export default pool;
