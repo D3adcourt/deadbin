2711086171617cd1267b76bcbbe347f642ff4ae2
alexrhett2010@gmail.com <alexrhett2010@gmail.com>
2026-07-21 18:38:37 -0400

    Changed DB

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'deadbin',
+  host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
+  port: 4000,
+  user: '2JryJAvVwJ6Leno.root',
+  password: 'kaRfKal7xZLxcRzJ',
+  database: 'test',
   waitForConnections: true,
   charset: 'utf8mb4'
 });
